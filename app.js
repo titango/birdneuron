c4bf4d60ecd721c7a3b6c386ed4fe6ee19358d28
CaoPhiHung <caophihung8392@gmail.com>
2019-05-20 16:34:45 -0700

    update port to 8080

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 8080;
 var index = require('./routes/index');
 app.use(express.static('public'));
 
 //add the router
 app.use('/', index);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || port);
 
-console.log('Running at Port 3000');
+console.log('Running at Port ' + port);
