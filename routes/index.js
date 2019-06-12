8bb61252664bc267ce41ee08ffeb8239fc32105d
Phi Hung Cao <caophihung8392@gmail.com>
2019-06-11 23:02:24 -0700

    Add main layout

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,29 @@ router.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
+router.get('/about_us',function(req,res){
+  res.sendFile(path.join(__dirname+'/../views/about_us.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/header',function(req,res){
+  res.sendFile(path.join(__dirname+'/../views/header.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/footer',function(req,res){
+  res.sendFile(path.join(__dirname+'/../views/footer.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/demo1',function(req,res){
+  res.sendFile(path.join(__dirname+'/../views/demo1.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/demo2',function(req,res){
+  res.sendFile(path.join(__dirname+'/../views/demo2.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
 module.exports = router;
