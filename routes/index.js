5fe5597bb6f64a8b9be245605a270646d4b67f6f
Phi Hung Cao <caophihung8392@gmail.com>
2019-06-23 16:08:26 -0700

    updated

--- routes/index.js
+++ routes/index.js
@@ -2,39 +2,34 @@ var express = require('express');
 const path = require('path');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
+//return the home page
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/../views/index.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
+//return the about-us page
 router.get('/about_us',function(req,res){
   res.sendFile(path.join(__dirname+'/../views/about_us.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
+//return the header section
 router.get('/header',function(req,res){
   res.sendFile(path.join(__dirname+'/../views/header.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
+//return the footer section
 router.get('/footer',function(req,res){
   res.sendFile(path.join(__dirname+'/../views/footer.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
+//return the first demo page
 router.get('/demo1',function(req,res){
   res.sendFile(path.join(__dirname+'/../views/demo1.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
+//return the second demo page
 router.get('/demo2',function(req,res){
   res.sendFile(path.join(__dirname+'/../views/demo2.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 module.exports = router;
